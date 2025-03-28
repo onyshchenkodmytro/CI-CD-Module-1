a524b203bf5280321a4997781bdea646142a7723
onyshchenkodmytro <dimonchakus@gmail.com>
2025-03-28 22:37:11 +0200

    Add function to sort data by population

--- population_sort.py
+++ population_sort.py
@@ -7,3 +7,7 @@ def read_population_data(filename):
 def sort_by_area(data):
     """Сортує дані за площею (від найбільшої до найменшої)."""
     return sorted(data, key=lambda x: x[1], reverse=True)
+
+def sort_by_population(data):
+    """Сортує дані за населенням (від найбільшого до найменшого)."""
+    return sorted(data, key=lambda x: x[2], reverse=True)
