28260e159e08667ced023036f8925670b9d08eed
onyshchenkodmytro <dimonchakus@gmail.com>
2025-03-28 23:13:11 +0200

    Add test report

--- tests/test_population_sort.py
+++ tests/test_population_sort.py
@@ -1,4 +1,7 @@
+import os
+import sys
 import pytest
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 from population_sort import read_population_data, sort_by_area, sort_by_population
 
 @pytest.fixture
