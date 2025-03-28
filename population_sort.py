82a4db9c1f1f4aa981fbcf172d06182b03937cc8
onyshchenkodmytro <dimonchakus@gmail.com>
2025-03-28 22:36:43 +0200

    Add function to sort data by area

--- population_sort.py
+++ population_sort.py
@@ -3,3 +3,7 @@ def read_population_data(filename):
     with open(filename, 'r', encoding='utf-8') as file:
         data = [line.strip().split(", ") for line in file.readlines()]
     return [(country, float(area), int(population)) for country, area, population in data]
+
+def sort_by_area(data):
+    """Сортує дані за площею (від найбільшої до найменшої)."""
+    return sorted(data, key=lambda x: x[1], reverse=True)
